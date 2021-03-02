c937d12a0d1e09c226b13c55347c2f46a9cc10ad
Omarja21 <jarrahom21@gmail.com>
2021-03-02 21:24:50 +0200

    this is the new update for the project

--- js/app.js
+++ js/app.js
@@ -1,37 +1,106 @@
-'use strict';
-function welcomMessage(){
+ 
     let Greeting =prompt("What is you Name Good sir?")
+    
     document.getElementById("welcome").innerHTML= "Hello " + Greeting + " nice to see this lovely name <3"
-}
-welcomMessage();
+
+
 alert("Please answer the questions with yes or no OR y/n");
-function askUsers(){
-let strOne= prompt("Do I love cookies?") 
-let strTwo= prompt("Am I over 18?")
-let strThree= prompt("Do you think I support  barcelona?")
-let strFour = prompt("Do you think I like mansaf?")
-let strFive = prompt("Do you think I love programming?")
+
+
+let count=0;  
+let strOne= prompt("Do I love cookies?");
+let strTwo= prompt("Am I over 18?");
+let strThree= prompt("Do you think I support  barcelona?");
+let strFour = prompt("Do you think I like mansaf?");
+let strFive = prompt("Do you think I love programming?");
+
+
 let QuestionOne= strOne.toUpperCase();
-let questionTwo= strTwo.toUpperCase();
-let questionThree= strThree.toUpperCase();
+let QuestionTwo= strTwo.toUpperCase();
+let QuestionThree= strThree.toUpperCase();
 let Questionfour= strFour.toUpperCase();
 let Questionfive= strFive.toUpperCase();
-if (QuestionOne=== "y", "yes"){
+
+
+if (QuestionOne== 'Y'|| QuestionOne=='YES' ){
     console.log(true);
-} else {console.log(false)}
-if (questionTwo=== "y", "yes" ){
-    console.log(true);
-} else {console.log(false)}
-if (questionThree=== "y", "yes"){
-    console.log(true);
-} else {console.log(false)}
-if (Questionfour=== "y", "yes","y".toUpperCase, "yes".toUpperCase){
-    console.log(true);
-} else {console.log(false)}
-if (Questionfive=== "y", "yes"){
-    console.log(true);
-} else {console.log(false)}
-document.getElementById("test").innerHTML= "And these were your answers for the Questions: "+ "<br>" +  "Do I love cookies?" + QuestionOne + "<br>" +" Am I over 18? " + questionTwo +"<br>" +
- " Do you think I support  barcelona? " + questionThree +"<br>" + " Do you think I like mansaf? " + Questionfour + "<br>" +" Do you think I love programming? " + Questionfive ; 
+    count++;
+} else {
+    console.log(false);
 }
-askUsers();
+
+if (QuestionTwo== 'Y'|| QuestionTwo=='YES' ){
+    console.log(true);
+    count++;
+} else {
+    console.log(false);
+}
+if (QuestionThree== 'Y'|| QuestionThree=='YES' ){
+    console.log(true);
+    count++;
+} else {
+    console.log(false);
+}
+if (Questionfour== 'Y'|| Questionfour=='YES' ){
+    console.log(true);
+    count++;
+} else {
+    console.log(false);
+}
+if (Questionfive== 'Y'|| Questionfive=='YES' ){
+    console.log(true);
+    count++;
+} else {
+    console.log(false);
+}
+document.getElementById("test").innerHTML= "And these were your answers for the Questions: "+ "<br>" +  "Do I love cookies?" + QuestionOne + "<br>" +" Am I over 18? " + QuestionTwo +"<br>" +
+ " Do you think I support  barcelona? " + QuestionThree +"<br>" + " Do you think I like mansaf? " + Questionfour + "<br>" +" Do you think I love programming? " + Questionfive ; 
+
+for(var i=1; i<=3; i++){
+    let strsix =  prompt("guess the number I like?");
+    let Questionsix = parseInt(strsix);
+    if (Questionsix>7){
+    alert("too high");
+    } else if(Questionsix<7){
+    alert("too low");
+    } else{
+        count++;
+        break;
+    }
+}
+alert('my lucky number is 7');
+alert("Now it is time for the guessing game about  what is my favourite color");
+var color=["BLUE", "CYAN", "YELLOW", "RED"];
+var flag= false;
+for (let i = 1; i <= 6; i++) {
+    let questionSeven=prompt("What is my favourite color");
+    switch(questionSeven.toUpperCase()){
+        case color[0]:
+            alert('correct answer');
+            flag=true;
+            count++;
+            break;
+        case color[1]:
+            alert('correct answer');
+            flag=true;
+            count++;
+            break;
+        case color[2]:
+            alert('correct answer');
+            flag=true;
+            count++;
+            break;
+        case color[3]:
+            alert('correct answer');
+            flag=true;
+            count++;
+            break;
+        default: 
+        alert('try again'); 
+        break;   
+    }
+    if(flag){  
+        break;
+    }
+}
+alert('you get '+ count+ ' of 7');
